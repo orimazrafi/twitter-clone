315cef2a41928169408ec12d6ce6bdafa42599ef
ori mazrafi <orimazrafi@gmail.com>
2020-07-28 14:54:42 +0300

    Handle profile user-card.

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -46,7 +46,6 @@ function App() {
                   />
                 )}
               />
-
               <Route exact path="/more" component={Views.More} />
             </Switch>
           </Col>
--- twitter-clone/src/views/Profile/Profile.js
+++ twitter-clone/src/views/Profile/Profile.js
@@ -1,27 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import style from "./style.module.scss";
 import { TweetModal } from "../../components/TweetModal/TweetModal";
 import { TabsGeneric } from "../../components/TabsGeneric/TabsGeneric";
-import { Link, Route } from "react-router-dom";
+import { Route, useHistory } from "react-router-dom";
 import { Tweet } from './../Tweet/Tweet';
 import { Media } from './../Media/Media';
-import { Likes } from './../Likes/Likes';
 import Switch from "react-bootstrap/esm/Switch";
 
 export const Profile = ({ tweets, onTweetSubmit }) => {
+  const history = useHistory();
+
   const [activeTab, setActiveTab] = useState("profile")
+  useEffect(() => {
+    history.push("/" + activeTab)
+  }, [activeTab, history])
   const [modalShow, setModalShow] = useState(false);
   const [tweet, setTweet] = useState("");
   const handleTweet = (value) => {
     setTweet(value);
   };
   const handleActiveTab = (tab) => {
-    console.log({ tab })
-    setTimeout(() => {
-
-      setActiveTab(tab)
-    }, 0)
+    console.log({ tab, activeTab })
+    setActiveTab(tab)
   }
   const tweetTheTweet = () => {
     onTweetSubmit(tweet);
@@ -36,6 +37,7 @@ export const Profile = ({ tweets, onTweetSubmit }) => {
 
   return (
     <>
+      <UserCard tweets={tweets} />
       <TabsGeneric defaultKey={"profile"} >
         <Container className={style.tabs_generic}>
           <Row>
@@ -49,7 +51,6 @@ export const Profile = ({ tweets, onTweetSubmit }) => {
       </TabsGeneric>
       <Container fluid className={style.profile}>
         <Switch>
-
           <Route exact path="/profile" render={() => <Tweet tweets={tweets} onLike={handleLike} onAddToTweet={handleTweetModal} />} />
           <Route path="/profile/replies" render={() => <Tweet tweets={tweets} onLike={handleLike} onAddToTweet={handleTweetModal} />} />
           <Route path="/profile/media" render={() => <Media
@@ -84,8 +85,20 @@ export const Profile = ({ tweets, onTweetSubmit }) => {
 };
 
 const TabItem = (props) => <Col xs={props.size} className={style.link_wrapper} style={{ textAlign: "center", padding: "15px" }} onClick={() => props.handleActiveTab(props.tabName)}>
-  <Link to={`/${props.tabName}`} className={props.activeTab === props.tabName ? style.active_link : style.link} >{props.tabText}</Link>
+  {console.log(`/${props.tabName}`)}
+  <div className={props.activeTab === props.tabName ? style.active_link : style.link} >{props.tabText}</div>
   {props.activeTab === props.tabName &&
     <hr />
   }
 </Col>
+
+const UserCard = ({ tweets }) => <Container className={style.user_card}>
+  <Row className={style.inner_wrapper} >
+    <Col xs={1} className={style.arrow_wrapper}><svg viewBox="0 0 24 24" ><g><path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path></g></svg></Col>
+    <Col style={{ marginLeft: "10px" }}>
+      <Col><h2 className={style.headline} > Ori Mazrafi</h2></Col>
+      {tweets.length !== 0 ? <Col className={style.text_muted} >{tweets.length} Tweet{tweets.length > 1 && "s"}</Col> :
+        <Col className={style.text_muted}>There are no Tweets  for this user yet!</Col>}
+    </Col>
+  </Row>
+</Container>
